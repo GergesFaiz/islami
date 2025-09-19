809a2c0d56c0c98a6cc87f42578511d35c0762f4
gero_ <gergesfaiz3@gmail.com>
2025-09-19 10:16:02 +0300

    app init

---
+++ lib/home/home.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:islami/home/tabs/ahadeth_tab.dart';
+import 'package:islami/home/tabs/quran_tab.dart';
+import 'package:islami/home/tabs/radio_tab.dart';
+import 'package:islami/home/tabs/sebha_tab.dart';
+import 'package:islami/home/tabs/settings_tab.dart';
+
+class HomeScreen extends StatefulWidget {
+  static const String routeName = "Home";
+
+  HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int index = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Image.asset("assets/images/background.png",width: double.infinity,),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            centerTitle: true,
+            title: Text(
+              "إسلامي",
+              style: GoogleFonts.elMessiri(
+                fontWeight: FontWeight.w700,
+                fontSize: 30,
+              ),
+            ),
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            backgroundColor: Color(0xFFB7935F),
+            type: BottomNavigationBarType.fixed,
+            currentIndex: index,
+            selectedItemColor: Colors.white,
+            unselectedItemColor: Colors.black38,
+            elevation: 0,
+            iconSize: 35,
+            onTap: (value) {
+              index = value;
+              setState(() {});
+            },
+
+            items: [
+              BottomNavigationBarItem(
+                icon: ImageIcon(AssetImage("assets/images/ic_radio.png")),
+                label: "add",
+              ),
+              BottomNavigationBarItem(
+                icon: ImageIcon(AssetImage("assets/images/ic_sebha.png")),
+                label: "add",
+              ),
+              BottomNavigationBarItem(
+                icon: ImageIcon(AssetImage("assets/images/ic_ahadeth.png")),
+                label: "add",
+              ),
+              BottomNavigationBarItem(
+                icon: ImageIcon(AssetImage("assets/images/ic_quran.png")),
+                label: "add",
+              ),
+              BottomNavigationBarItem(icon: Icon(Icons.settings), label: "mi"),
+            ],
+          ),
+          body: tabs[index],
+        ),
+      ],
+    );
+  }
+
+  List<Widget> tabs = [
+    RadioTab(),
+    SebhaTab(),
+    AhadethTab(),
+    QuranTab(),
+    SettingsTab(),
+  ];
+}
